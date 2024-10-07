a1997f2861a68f1736cfd12de2a4ccb0a1d48e1c
Ivan Tumashevskiy <itumashevskiy@me.com>
2024-10-07 17:20:24 +0200

    Start of Calc app
    First ever Python commit with two method for a future Calc app.

---
+++ BasicCalculator/calculator.py
@@ -0,0 +1,6 @@
+def add(x, y):      # method of addition, which takes two parameters x and y and returns the addition of them
+    return x + y
+
+def subtract(x, y):     # method of subtraction of two numbers
+    return x - y
+
