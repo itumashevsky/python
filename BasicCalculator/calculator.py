17af3eaffef1165c1824f2954f558bdcc7c50847
Ivan Tumashevskiy <itumashevskiy@me.com>
2024-10-08 17:54:35 +0200

    Adding two more methods

--- BasicCalculator/calculator.py
+++ BasicCalculator/calculator.py
@@ -4,3 +4,13 @@ def add(x, y):      # method of addition, which takes two parameters x and y and
 def subtract(x, y):     # method of subtraction of two numbers
     return x - y
 
+def multiply(x, y):
+    return x * y
+
+def divide(x, y)
+    if y == 0
+        return "Can not divide by zero!"
+    return x/y
+
+def main():
+    while True:
